e5113d31bed0cfb688e582354882aac94fd3412b
YaaQA1004 <yaaqa1004@gmail.com>
2024-05-01 12:30:21 -0700

    i created new definition file

---
+++ src/test/java/definitions/HelenStepDefs.java
@@ -0,0 +1,9 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+
+public class HelenStepDefs {
+    @Given("I navigate to {string} section on ask-qa.portnov.com")
+    public void iNavigateToSectionOnAskQaPortnovCom(String arg0) {
+    }
+}
--- src/test/resources/features/helen.feature
+++ src/test/resources/features/helen.feature
@@ -5,3 +5,7 @@ Feature: Git Practice
   @git1
   Scenario: My first git commands
   Given
+
+  @git2
+  Scenario: ASK - Registration Last Name
+    Given I navigate to "registration" section on ask-qa.portnov.com
