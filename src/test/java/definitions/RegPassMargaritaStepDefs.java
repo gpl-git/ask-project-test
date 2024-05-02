1c3a18908a5ca42725b3abc4a8cecc0ba58e5e98
margarita-mi <mozharova.m@gmail.com>
2024-05-01 23:04:46 -0700

    created .feature and .java files/created all scenarios and definitions

---
+++ src/test/java/definitions/RegPassMargaritaStepDefs.java
@@ -0,0 +1,46 @@
+package definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static support.TestContext.getDriver;
+
+public class RegPassMargaritaStepDefs {
+    @And("I type {string} into confirm password field")
+    public void iTypeIntoConfirmPasswordField(String confPassword) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='confirmPassword']")).sendKeys(confPassword);
+    }
+
+
+    @Then("error message under confirm password field should be displayed")
+    public void errorMessageUnderConfirmPasswordFieldShouldBeDisplayed() {
+        assertThat(getDriver().findElement(By.xpath("//input[@formcontrolname='confirmPassword']/../../..//mat-error")).isDisplayed()).isTrue();
+    }
+
+    @And("error message under confirm password field should be as {string}")
+    public void errorMessageUnderConfirmPasswordFieldShouldBeAs(String expectedConfError) {
+        String actualConfError = getDriver().findElement(By.xpath("//input[@formcontrolname='confirmPassword']/../../..//mat-error")).getText();
+        System.out.println(actualConfError);
+        assertThat(actualConfError.equals(expectedConfError)).isTrue();
+    }
+
+
+    @And("registration is not processed")
+    public void registrationIsNotProcessed() {
+        assertThat(getDriver().findElement(By.xpath("//h4[contains(text(),'Registration')]")).isDisplayed()).isTrue();
+    }
+
+    @Then("error message under password field should be displayed")
+    public void errorMessageUnderPasswordFieldShouldBeDisplayed() {
+        assertThat(getDriver().findElement(By.xpath("//input[@formcontrolname='password']/../../..//mat-error")).isDisplayed()).isTrue();
+    }
+
+    @And("error message under password field should be as {string}")
+    public void errorMessageUnderPasswordFieldShouldBeAs(String expectedPassError) {
+        String actualPassError = getDriver().findElement(By.xpath("//input[@formcontrolname='password']/../../..//mat-error")).getText();
+        System.out.println(actualPassError);
+        assertThat(actualPassError.equals(expectedPassError)).isTrue();
+    }
+}
