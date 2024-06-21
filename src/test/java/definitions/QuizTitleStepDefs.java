c5a0fb3b9bdca13cc572789c1bc8dfc4b149fa5e
deepa280206 <deepa280206@gmail.com>
2024-06-20 17:36:59 -0700

    Deepa - Modified @quiz_title3 and added theQuizShouldNotBeDisplayedOnTheListOfQuizzes

--- src/test/java/definitions/QuizTitleStepDefs.java
+++ src/test/java/definitions/QuizTitleStepDefs.java
@@ -1,9 +1,14 @@
 package definitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
 public class QuizTitleStepDefs {
@@ -13,6 +18,17 @@ public class QuizTitleStepDefs {
 
         for(int i=0;i < quizTitle.length();i++) {
             getDriver().findElement(By.xpath("//input[@formcontrolname='name']")).sendKeys(Keys.BACK_SPACE);
+        }
+    }
+
+    @And("the quiz {string} should not be displayed on the list of quizzes")
+    public void theQuizShouldNotBeDisplayedOnTheListOfQuizzes(String quizTitle) {
+        List<WebElement> titles = getDriver().findElements(By.xpath("//mat-panel-title"));
+        for (WebElement title: titles) {
+            if(title.getText().contains(quizTitle)){
+                assertThat(title.isDisplayed()).isFalse();
+            }
+
         }
     }
 }
